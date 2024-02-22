38cf28f4babfe1bb08739137a08869cab293337e
rogers Lu <61651257+rogers228@users.noreply.github.com>
2024-02-22 15:06:32 +0800

    新增

--- sublime_hide.py
+++ sublime_hide.py
@@ -1,7 +1,8 @@
-# 此檔案放在個專案資料夾底下  用來控制資料夾，檔案的隱藏狀態，有2種執行方式
-# 1. 由ahk監控切換sublime專案後執行：註冊 C:\Users\user\Documents\Rogers\sublime_text_ffds\regist_project.json
-# 2. 儲存本設定檔後自動執行：此腳本由 SublimeOnSaveBuild (sublime套件) 設定為儲存後自動執行
-# 採用註解符號 # 來控制單引號'' 中的檔案名稱，進行隱藏，符合編程習慣
+#
+    # 此檔案放在個專案資料夾底下  用來控制資料夾，檔案的隱藏狀態，有2種執行方式
+    # 1. 由ahk監控切換sublime專案後執行：註冊 C:\Users\user\Documents\Rogers\sublime_text_ffds\regist_project.json
+    # 2. 儲存本設定檔後自動執行：此腳本由 SublimeOnSaveBuild (sublime套件) 設定為儲存後自動執行
+    # 採用註解符號 # 來控制單引號'' 中的檔案名稱，進行隱藏，符合編程習慣
 import os, subprocess
 
 is_hide = True  # True | False
@@ -27,6 +28,5 @@ def main():
     command = f'python sublime_setting.py -project {pj_name} -mode 1'
     cwd = r'C:\Users\user\Documents\Rogers\sublime_text_ffds'
     subprocess.run(command, cwd=cwd, shell=True, capture_output=True, text=True)
-
 if __name__ == '__main__':
     main()
